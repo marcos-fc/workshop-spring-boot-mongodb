b9d98af85e4e107bdd3796b6ee4b82a493ad5f3c
Marcos Fernandes <marcosfc.ads@gmail.com>
2022-07-23 07:50:42 -0300

    Projeto criado

---
+++ src/main/java/com/marcosfc/workshopmongodb/WorkshopSpringBootMongobdApplication.java
@@ -0,0 +1,13 @@
+package com.marcosfc.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongobdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongobdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosfc/workshopmongodb/WorkshopSpringBootMongobdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosfc.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongobdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
