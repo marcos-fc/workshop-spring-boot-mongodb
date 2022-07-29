e4f891e0f506f2a02a6b509a2938e19bbd3faf6d
Marcos Fernandes <marcosfc.ads@gmail.com>
2022-07-29 12:18:28 -0300

    Obtendo um usuário por id

--- src/main/java/com/marcosfc/workshopmongodb/services/UserService.java
+++ src/main/java/com/marcosfc/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marcosfc.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcosfc.workshopmongodb.domain.User;
 import com.marcosfc.workshopmongodb.repository.UserRepository;
+import com.marcosfc.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findbyId(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
